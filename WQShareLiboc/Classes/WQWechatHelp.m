16b37f4fe141a7c5726dbc712c04ea7cc4b8074a
huwenqiang <hwqforapple@163.com>
2019-04-16 15:34:44 +0800

    微信缩略图压缩

--- WQShareLiboc/Classes/WQWechatHelp.m
+++ WQShareLiboc/Classes/WQWechatHelp.m
@@ -50,10 +50,50 @@
     WXMediaMessage *message = [WXMediaMessage message];
     message.title = title;
     message.description = description;
+    thumbImage = [self compressImage:thumbImage toByte:27*1024];
     [message setThumbImage:thumbImage];
     message.mediaObject = webobject;
     [self sendMessage:message shareType:type];
     return  WQShareReturnTypeSucceed;
+}
+
++ (UIImage *)compressImage:(UIImage *)image toByte:(NSUInteger)maxLength {
+    // Compress by quality
+    CGFloat compression = 1;
+    NSData *data = UIImageJPEGRepresentation(image, compression);
+    if (data.length < maxLength) return image;
+    
+    CGFloat max = 1;
+    CGFloat min = 0;
+    for (int i = 0; i < 6; ++i) {
+        compression = (max + min) / 2;
+        data = UIImageJPEGRepresentation(image, compression);
+        if (data.length < maxLength * 0.9) {
+            min = compression;
+        } else if (data.length > maxLength) {
+            max = compression;
+        } else {
+            break;
+        }
+    }
+    UIImage *resultImage = [UIImage imageWithData:data];
+    if (data.length < maxLength) return resultImage;
+    
+    // Compress by size
+    NSUInteger lastDataLength = 0;
+    while (data.length > maxLength && data.length != lastDataLength) {
+        lastDataLength = data.length;
+        CGFloat ratio = (CGFloat)maxLength / data.length;
+        CGSize size = CGSizeMake((NSUInteger)(resultImage.size.width * sqrtf(ratio)),
+                                 (NSUInteger)(resultImage.size.height * sqrtf(ratio))); // Use NSUInteger to prevent white blank
+        UIGraphicsBeginImageContext(size);
+        [resultImage drawInRect:CGRectMake(0, 0, size.width, size.height)];
+        resultImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        data = UIImageJPEGRepresentation(resultImage, compression);
+    }
+    
+    return resultImage;
 }
 
 /**发送媒体类消息*/
@@ -72,7 +112,7 @@
         { req.scene = WXSceneFavorite;}
             break;
         default:
-             req.scene = WXSceneTimeline;
+            req.scene = WXSceneTimeline;
             break;
     }
     [WXApi sendReq:req];
